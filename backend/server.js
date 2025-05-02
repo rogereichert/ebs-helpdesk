13295b45e7e79a021adab86db715f18c92dd0261
Roger <rogereichert@gmail.com>
2025-05-02 12:01:09 -0300

    Foram criados os arquivos: db.js, server.js e .env

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+// Carrega as variáveis do ambiente
+require('dotenv').config()
+
+// Importa os módulos necessários
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+// Importa a conexão com o banco de dados
+const db = require('./config/db')
+
+// Middlewares globais
+app.use(cors()) // Permite requisições de diferentes origens
+app.use(express.json()) // Converte o body das requisições para JSON
+
+// Testa a conexão com banco de dados ao iniciar
+db.connect((err) => {
+    if (err) {
+        console.log('Erro ao conectar ao banco de dados')
+        process.exit(1) // Encerra
+    }
+    console.log('Conectado ao banco de dados MySql')
+})
+
+// Rotas
+app.get('/', (req, res) => {
+    res.send('API de chamados iniciada com sucesso')
+})
+
+// Porta inicial do servidor
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
